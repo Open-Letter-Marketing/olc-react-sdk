d37bb092c4ad36074fa4c6e8a6f86cc021513b13
ZainAliOlm <zain@openletterconnect.com>
2024-10-08 17:56:21 +0500

    Minor Update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,6 +17,7 @@ export const multiPageLetters: string[] = [
   "Bi-Fold Self-Mailers",
 ];
 
+//@ts-ignore
 export const emojiRegex = /[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{2300}-\u{23FF}]|[\u{2B50}\u{2B55}]|[\u{1F004}\u{1F0CF}]/u;
 
 interface TemplateTypes {
