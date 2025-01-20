85edb2bbc5b6fe46a57e6b5430c76e1db75779ee
ZainAliOlm <zain@openletterconnect.com>
2025-01-20 18:46:50 +0500

    Fixed Issue: Sequence count number incorrectly displayed when switching between Professional Letter

--- src/components/CreateTemplate/index.tsx
+++ src/components/CreateTemplate/index.tsx
@@ -173,6 +173,11 @@ const CreateTemplate: React.FC<CreateTemplateProps> = ({ onReturnAndNavigate, cr
           dispatch(
             selectProduct(sortedProducts.find((item) => item.windowed === false))
           );
+        //@ts-ignore
+        } else if(envelopeType?.label === 'Windowed Envelope') {
+          dispatch(
+            selectProduct(sortedProducts.find((item) => item.windowed === true))
+          );
         }
       }
     }, [envelopeType]);
