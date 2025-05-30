376af114fa8b0f3fed8a03b67ace0c9460f76aa5
Usman Iqbal <usman22.cs@gmail.com>
2025-05-30 23:40:26 +0500

    Feature: Add TemplatesCard component for displaying and managing templates in the SidePanel

--- src/components/SidePanel/Templates/TemplatesCard/index.tsx
+++ src/components/SidePanel/Templates/TemplatesCard/index.tsx
@@ -6,8 +6,8 @@ import {MESSAGES} from '../../../../utils/message';
 // // icons
 import Dot from '../../../../assets/images/templates/dot';
 import ArrowDown from '../../../../assets/images/templates/arrow-down';
-import Button from '../../../../components/GenericUIBlocks/Button';
-import Typography from '../../../../components/GenericUIBlocks/Typography';
+import Button from '../../../GenericUIBlocks/Button';
+import Typography from '../../../GenericUIBlocks/Typography';
 
 // styles
 import './styles.scss';
@@ -22,7 +22,7 @@ const TemplatesCard = (props: any) => {
     product,
     searchApplied,
     primaryColorRGBA,
-    designerQueryAmount
+    designerQueryAmount,
   } = props;
 
   const [isFilpedIds, setIsFlipedIds] = useState<string[]>([]);
@@ -119,17 +119,33 @@ const TemplatesCard = (props: any) => {
           );
         })
       ) : currentTemplateType?.id === '1' ? (
-        <div className={`${+product?.id === 5 && designerQueryAmount ? 'noTemplateGallery more-visible' :
-        product?.productType === "Professional Letters" ? 'noTemplateGallery noTemplateProfessionalAndPersonal' : 'noTemplateGallery'}`}>
+        <div
+          className={`${
+            +product?.id === 5 && designerQueryAmount
+              ? 'noTemplateGallery more-visible'
+              : product?.productType === 'Professional Letters'
+                ? 'noTemplateGallery noTemplateProfessionalAndPersonal'
+                : 'noTemplateGallery'
+          }`}
+        >
           <Typography>{MESSAGES.TEMPLATE.NO_MY_TEMPLATES}</Typography>
         </div>
       ) : currentTemplateType?.id === '2' ? (
-        <div className={`${+product?.id === 5 && designerQueryAmount ? 'noTemplateGallery more-visible' : 'noTemplateGallery'}`}>
+        <div
+          className={`${+product?.id === 5 && designerQueryAmount ? 'noTemplateGallery more-visible' : 'noTemplateGallery'}`}
+        >
           <Typography>{MESSAGES.TEMPLATE.NO_TEAM_TEMPLATES}</Typography>
         </div>
       ) : currentTemplateType?.id === '3' ? (
-        <div className={`${+product?.id === 5 && designerQueryAmount ? 'noTemplateGallery more-visible' : 
-        product?.productType === "Professional Letters" ? 'noTemplateGallery noTemplateProfessionalAndPersonal' : 'noTemplateGallery'}`}>
+        <div
+          className={`${
+            +product?.id === 5 && designerQueryAmount
+              ? 'noTemplateGallery more-visible'
+              : product?.productType === 'Professional Letters'
+                ? 'noTemplateGallery noTemplateProfessionalAndPersonal'
+                : 'noTemplateGallery'
+          }`}
+        >
           <Typography>
             {platformName
               ? `No ${platformName} Templates to show`
